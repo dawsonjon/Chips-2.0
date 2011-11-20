0a38f11b8ce84c30d998088649a702eb1764eba9
jon <jon@jon-Aspire-5738.(none)>
2011-11-20 16:51:59 +0000

    More improvements to schematic editor.

--- blocked.py
+++ blocked.py
@@ -3,22 +3,22 @@
 import wx
 from wx.lib.floatcanvas import NavCanvas, FloatCanvas, Resources
 
+import components
 
 class InPort:
     def __init__(self, blockframe, position, label):
         self.blockframe = blockframe
         self.connected = False
+        self.position = position
         x, y = position
-        self.rectangle = self.blockframe.canvas.AddPolygon(
-            [
-                (x, y+7), 
-                (x+7, y),
-                (x, y-7), 
-            ],
-            LineColor = "dark green",
+        self.outline = self.blockframe.canvas.AddCircle(
+            (x-10, y), 
+            10,
+            LineColor = "red",
             LineWidth = 1
         )
-        self.rectangle.Bind(
+        self.outline.PutInForeground()
+        self.outline.Bind(
                 FloatCanvas.EVT_FC_LEFT_DOWN, 
                 lambda obj: self.blockframe.on_in_port_left_down(self)
         )
@@ -29,18 +29,17 @@ class InPort:
                 10,
                 Position = "cl",
             )
-        self.position = position
 
-    def move(self, coord):
-        self.position = coord
-        x, y = coord
-        self.rectangle.SetPoints(
-            [
-                (x, y+7), 
-                (x+7, y),
-                (x, y-7), 
-            ],
-        )
+    def hide(self):
+        self.outline.Hide()
+
+    def show(self):
+        self.outline.Show()
+
+    def move(self, postion):
+        self.position = postion
+        x, y = postion
+        self.outline.SetPoint((x-10, y))
         if self.connected:
             self.out_port.draw_wire()
         if hasattr(self, "label"):
@@ -49,7 +48,7 @@ class InPort:
             )
 
     def delete(self):
-        self.blockframe.canvas.RemoveObject(self.rectangle)
+        self.blockframe.canvas.RemoveObject(self.outline)
         if self.connected:
             self.out_port.disconnect()
         if hasattr(self, "label"):
@@ -61,43 +60,40 @@ class OutPort:
         self.connected = False
         self.blockframe = blockframe
         x, y = position
-        self.rectangle = self.blockframe.canvas.AddPolygon(
-            [
-                (x-7, y+7), 
-                (x, y),
-                (x-7, y-7), 
-            ],
-            LineColor = "dark green",
-            LineWidth = 1
+        self.position = position
+        self.outline = self.blockframe.canvas.AddCircle(
+            (x+10, y),
+            10,
+            LineColor = "red",
         )
-        self.rectangle.Bind(
+        self.outline.PutInForeground()
+        self.outline.Bind(
                 FloatCanvas.EVT_FC_LEFT_DOWN, 
                 lambda obj: self.blockframe.on_out_port_left_down(self)
         )
         if label is not None:
             self.label = self.blockframe.canvas.AddScaledText(
                     label,
-                    (x-10, y),
+                    (x-5, y),
                     10,
                     Position = "cr"
             )
-        self.position = position
 
     def draw_wire(self):
         if hasattr(self, "in_port"):
             self.wire.SetPoints([self.position, self.in_port.position])
             self.blockframe.canvas.Draw(True)
 
-    def move(self, coord):
-        self.position = coord
-        x, y = coord
-        self.rectangle.SetPoints(
-            [
-                (x-7, y+7), 
-                (x, y),
-                (x-7, y-7), 
-            ],
-        )
+    def hide(self):
+        self.outline.Hide()
+
+    def show(self):
+        self.outline.Show()
+
+    def move(self, postion):
+        self.position = postion
+        x, y = postion
+        self.outline.SetPoint((x+10, y))
         if hasattr(self, "label"):
             self.label.SetPoint(
                 (x-10, y),
@@ -105,7 +101,7 @@ class OutPort:
         self.draw_wire()
 
     def delete(self):
-        self.blockframe.canvas.RemoveObject(self.rectangle)
+        self.blockframe.canvas.RemoveObject(self.outline)
         if self.connected:
             self.disconnect()
         if hasattr(self, "label"):
@@ -115,7 +111,9 @@ class OutPort:
     def disconnect(self):
         if self.connected:
             self.in_port.connected = False
+            self.in_port.show()
             self.connected = False
+            self.show()
             del self.in_port.out_port
             del self.in_port
             self.blockframe.canvas.RemoveObject(self.wire)
@@ -127,97 +125,114 @@ class OutPort:
         else:
             self.connected = True
             self.in_port = in_port
+            self.hide()
             self.in_port.connected = True
+            self.in_port.hide()
             self.in_port.out_port = self
             self.wire = self.blockframe.canvas.AddLine(
                     [self.position, self.in_port.position],
-                    LineColor = "dark green",
-                    LineWidth = 3
+                    LineColor = "green",
+                    LineWidth = 2,
             )
+            self.wire.HitLineWidth = 6
+            self.wire.PutInBackground()
             self.wire.Bind(
                     FloatCanvas.EVT_FC_RIGHT_DOWN, 
                     lambda obj: self.blockframe.on_wire_right_down(self)
             )
             self.blockframe.canvas.Draw(True)
 
-class WireHandle:
+class Bend:
     def __init__(self, blockframe, position):
         x, y = position
         self.blockframe = blockframe
-        self.rectangle = self.blockframe.canvas.AddPolygon(
-            [
-                (x, y+20),
-                (x+10, y+20),
-                (x+10, y-20),
-                (x, y-20),
-                (x, y-10),
-                (x-10, y-10),
-                (x-10, y+10),
-                (x, y+10),
-            ],
-            LineColor = "dark green"
+        self.outline = self.blockframe.canvas.AddCircle(
+            position,
+            10,
+            LineColor = "green"
         )
-        self.inport = InPort(blockframe, (x-10, y), None)
-        self.outport1 = OutPort(blockframe, (x+10, y+10), None)
-        self.outport2 = OutPort(blockframe, (x+10, y-10), None)
-        self.rectangle.Bind(
+        self.outline.PutInForeground()
+        self.inport = InPort(blockframe, position, None)
+        self.outport = OutPort(blockframe, position, None)
+        self.outline.Bind(
             FloatCanvas.EVT_FC_LEFT_DOWN, 
             lambda obj : blockframe.on_block_left_down(self)
         )
-        self.rectangle.Bind(
+        self.outline.Bind(
             FloatCanvas.EVT_FC_RIGHT_DOWN, 
             lambda obj : blockframe.on_block_right_down(self)
         )
 
     def delete(self):
-        self.blockframe.canvas.RemoveObject(self.rectangle)
+        self.blockframe.canvas.RemoveObject(self.outline)
+        self.inport.delete()
+        self.outport.delete()
+        self.blockframe.canvas.Draw(True)
+
+    def move(self, position):
+        self.outline.SetPoint(position)
+        self.inport.move(position)
+        self.outport.move(position)
+        self.blockframe.canvas.Draw(True)
+
+class Tee:
+    def __init__(self, blockframe, position):
+        self.blockframe = blockframe
+        self.outline = self.blockframe.canvas.AddCircle(
+            position,
+            10,
+            FillColor = "green",
+            LineColor = "green"
+        )
+        self.outline.PutInForeground()
+        self.inport = InPort(blockframe, position, None)
+        self.outport1 = OutPort(blockframe, position, None)
+        self.outport2 = OutPort(blockframe, position, None)
+        self.outline.Bind(
+            FloatCanvas.EVT_FC_LEFT_DOWN, 
+            lambda obj : blockframe.on_block_left_down(self)
+        )
+        self.outline.Bind(
+            FloatCanvas.EVT_FC_RIGHT_DOWN, 
+            lambda obj : blockframe.on_block_right_down(self)
+        )
+
+    def delete(self):
+        self.blockframe.canvas.RemoveObject(self.outline)
         self.inport.delete()
         self.outport1.delete()
         self.outport2.delete()
         self.blockframe.canvas.Draw(True)
 
     def move(self, position):
-        x, y = position
-        if (self.outport1.connected and 
-                self.outport2.connected and
-                self.outport2.in_port.position[1] >
-                self.outport1.in_port.position[1]):
-            self.outport1, self.outport2 = self.outport2, self.outport1
-        self.rectangle.SetPoints(
-            [
-                (x, y+20),
-                (x+10, y+20),
-                (x+10, y-20),
-                (x, y-20),
-                (x, y-10),
-                (x-10, y-10),
-                (x-10, y+10),
-                (x, y+10),
-            ]
-        )
-
-        self.inport.move((x-10, y))
-        self.outport1.move((x+10, y+10))
-        self.outport2.move((x+10, y-10))
+        self.outline.SetPoint(position)
+        self.inport.move(position)
+        self.outport1.move(position)
+        self.outport2.move(position)
         self.blockframe.canvas.Draw(True)
 
 class Block:
-    def __init__(self, blockframe, position, input_ports, output_ports, title):
+    def __init__(self, blockframe, position, component):
+        title, input_ports, output_ports = component
         self.blockframe = blockframe
+        self.blockframe.blocks.append(self)
         x, y = position
 
         self.height = max([len(input_ports), len(output_ports)])*20 + 20
-        self.width = max((
-            max([len(i) for i in input_ports]) +
-            max([len(i) for i in output_ports])
-        ) * 10, 100)
+        self.width = max(
+            (
+                max([len(i) for i in input_ports] + [0]) +
+                max([len(i) for i in output_ports] + [0])
+            ) * 10, 
+            len(title) * 10,
+            100
+        )
 
-        self.rectangle = self.blockframe.canvas.AddRectangle(
+        self.outline = self.blockframe.canvas.AddRectangle(
                 position, 
                 (self.width, self.height+20),
                 LineWidth = 3,
-                LineColor = "dark green",
-                FillColor = "light green"
+                LineColor = "blue",
         )
 
         self.title = self.blockframe.canvas.AddScaledText(
@@ -226,12 +241,12 @@ class Block:
                 10
         )
 
-        self.rectangle.Bind(
+        self.outline.Bind(
             FloatCanvas.EVT_FC_LEFT_DOWN, 
             lambda obj : blockframe.on_block_left_down(self)
         )
 
-        self.rectangle.Bind(
+        self.outline.Bind(
             FloatCanvas.EVT_FC_RIGHT_DOWN, 
             lambda obj : blockframe.on_block_right_down(self)
         )
@@ -263,18 +278,19 @@ class Block:
         self.blockframe.canvas.Draw(True)
 
     def delete(self):
-        self.blockframe.canvas.RemoveObject(self.rectangle)
+        self.blockframe.canvas.RemoveObject(self.outline)
         self.blockframe.canvas.RemoveObject(self.title)
         for i in self.input_ports:
             i.delete()
         for i in self.output_ports:
             i.delete()
         self.blockframe.canvas.Draw(True)
+        self.blockframe.blocks.remove(self)
 
-    def move(self, coord):
-        x, y = coord
+    def move(self, postion):
+        x, y = postion
         x, y = x - 50 , y - self.height/2
-        self.rectangle.SetPoint((x, y))
+        self.outline.SetPoint((x, y))
         y_offset = self.height - 20
         for i in self.input_ports:
             i.move((x, y+y_offset)) 
@@ -288,6 +304,10 @@ class Block:
         )
         self.blockframe.canvas.Draw(True)
 
+            
+
+
+
 class BlockFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
 
@@ -298,8 +318,16 @@ class BlockFrame(wx.Frame):
         canvas = NavCanvas.NavCanvas(self, style=wx.SUNKEN_BORDER)
         self.canvas = canvas.Canvas
 
-        #make selector
-        selector = wx.TreeCtrl(self, style=wx.SUNKEN_BORDER)
+        #make component selector
+        selector = wx.TreeCtrl(self, style=wx.SUNKEN_BORDER|wx.TR_DEFAULT_STYLE)
+        root = selector.AddRoot("items")
+        for library_name, library in components.components:
+            library_node = selector.AppendItem(root, library_name)
+            for component in library:
+                title = component[0]
+                node = selector.AppendItem(library_node, title)
+                selector.SetItemPyData(node, component)
+        self.selector = selector
 
         #layout windows
         sizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -311,17 +339,33 @@ class BlockFrame(wx.Frame):
         self.state = "idle"
         self.canvas.Bind(FloatCanvas.EVT_LEFT_UP, self.on_left_up)
         self.canvas.Bind(FloatCanvas.EVT_LEFT_DOWN, self.on_left_down)
+        self.canvas.Bind(FloatCanvas.EVT_RIGHT_DOWN, self.on_right_down)
         self.canvas.Bind(FloatCanvas.EVT_MOTION, self.on_motion)
+
+        self.blocks = []
 
     def on_left_down(self, event):
         if self.state == "idle":
-            Block(self, event.Coords, ["a", "b", "c"], ["blah"], "a block")
+            component = self.selector.GetSelection()
+            component = self.selector.GetItemPyData(component)
+            if component is not None:
+                title, inputs, outputs = component
+                Block(self, event.Coords, component)
         elif self.state == "join_port":
-            handle = WireHandle(self, event.Coords)
+            handle = Bend(self, event.Coords)
             self.port_from.connect(handle.inport)
             self.canvas.RemoveObject(self.trace)
             self.canvas.Draw(True)
             self.state = "idle"
+
+    def on_right_down(self, event):
+        if self.state == "idle":
+            menu = wx.Menu()
+            self.Bind(wx.EVT_MENU, 
+                lambda evt: self.export(),
+                menu.Append(-1, "export"),
+            )
+            self.PopupMenu(menu)
 
     def on_left_up(self, event):
         if self.state == "move_block":
@@ -376,18 +420,33 @@ class BlockFrame(wx.Frame):
                 menu.Append(-1, "delete"),
             )
             self.Bind(wx.EVT_MENU, 
-                lambda evt: self.split(port),
-                menu.Append(-1, "split"),
+                lambda evt: self.tee(port),
+                menu.Append(-1, "tee"),
+            )
+            self.Bind(wx.EVT_MENU, 
+                lambda evt: self.bend(port),
+                menu.Append(-1, "bend"),
             )
             self.PopupMenu(menu)
 
-    def split(self, from_port):
+    def bend(self, from_port):
         to_port = from_port.in_port
+        x1, y1 = from_port.position 
+        x2, y2 = to_port.position
         from_port.disconnect()
-        x1, y1, x2, y2 = from_port.position + to_port.position
-        handle = WireHandle(self, ((x1+x2)/2, (y1+y2)/2))
+        handle = Bend(self, ((x1+x2)/2, (y1+y2)/2))
+        from_port.connect(handle.inport)
+        handle.outport.connect(to_port)
+
+    def tee(self, from_port):
+        to_port = from_port.in_port
+        x1, y1  = from_port.position 
+        x2, y2 = to_port.position
+        from_port.disconnect()
+        handle = Tee(self, ((x1+x2)/2, (y1+y2)/2))
         from_port.connect(handle.inport)
         handle.outport1.connect(to_port)
+
 
 app = wx.App()
 BlockFrame(None, size=(1024,768), title="Schematic editor").Show()
---
+++ components.py
@@ -0,0 +1,16 @@
+components = (
+	("Sources", 
+        (
+            ("Repeat Value", [], ["z"]),
+            ("Input Port", [], ["z"]),
+            ("Serial Input", [], ["serial data"]),
+        ),
+    ),
+	("Sinks" , 
+       (
+            ("Output Port", ["a"], []),
+            ("Serial Output", ["serial data"], []),
+            ("Console", ["console data"], []),
+       ),
+    ),
+)
