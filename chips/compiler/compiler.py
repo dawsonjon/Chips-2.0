aba16d52e610ba4189b0cf591fda5d072888a05e
Jon Dawson <chips@jondawson.org.uk>
2015-09-03 19:48:32 +0100

    make componenet names more repeatable

--- chips/compiler/compiler.py
+++ chips/compiler/compiler.py
@@ -17,6 +17,14 @@ from chips.compiler.verilog_area import generate_CHIP as generate_CHIP_area
 from chips.compiler.python_model import generate_python_model
 import fpu
 
+sn = 0
+def serialise(name):
+    global sn
+    retval = sn
+    sn += 1
+    return name + "_" + str(sn)
+
+
 def generate_library():
     output_file = open("chips_lib.v", "w")
     output_file.write(fpu.adder)
@@ -43,7 +51,7 @@ def comp(input_file, options=[], parameters={}):
             #Optimize for area
             parser = Parser(input_file, reuse, initialize_memory, parameters)
             process = parser.parse_process()
-            name = process.main.name + "_" + str(id(process)) 
+            name = serialise(process.main.name) 
             instructions = process.generate()
             instructions = expand_macros(instructions, parser.allocator)
             if "dump" in options:
@@ -84,7 +92,7 @@ def compile_python_model(
             #Optimize for area
             parser = Parser(input_file, False, False, parameters)
             process = parser.parse_process()
-            name = process.main.name + "_" + str(id(process)) 
+            name = serialise(process.main.name) 
             instructions = process.generate()
             instructions = expand_macros(instructions, parser.allocator)
             if "dump" in options:
