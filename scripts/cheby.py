74fe69bcfaa17ec64b004c93d373683ba816f222
Jon Dawson <chips@jondawson.org.uk>
2016-11-09 18:54:25 +0000

    update cheby.py

--- scripts/cheby.py
+++ scripts/cheby.py
@@ -18,3 +18,13 @@ polynomial, error = chebyfit(lambda x: 1+log(x, 2.0), [1.0, 2.0], 3, True)
 print "log2(x)"
 print error
 print reduce(horner, polynomial)
+
+polynomial, error = chebyfit(lambda x: sqrt(x), [1.0, 2.0], 8, True)
+print "sqrt(x)"
+print error
+print reduce(horner, polynomial)
+
+polynomial, error = chebyfit(lambda x: cos(x), [0, pi/2], 7, True)
+print "cos(x)"
+print error
+print reduce(horner, polynomial)
